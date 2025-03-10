e3a925d7858d732ec73ec2a852d20d3472651d04
kickthemoon <soonch67890@gmail>
2025-03-10 12:41:58 +0900

    필수 2단계 완료

--- src/kioskBurger/Main.java
+++ src/kioskBurger/Main.java
@@ -1,44 +1,43 @@
 package kioskBurger;
 
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        List<MenuItem> menuItems = new ArrayList<>();
 
         int chooseMain;
         boolean turnOff = false;
 
+        menuItems.add(new MenuItem("ShackBurger   ",6.9,"토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("SmokeShack    ",8.9,"베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Cheeseburger   ",7.2,"포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Hamburger   ",5.4,"비프패티를 기반으로 야채가 들어간 기본버거"));
+
         do {
             System.out.println("[ SHAKESHACK MENU ]");
-            System.out.println("1. ShackBurger");
-            System.out.println("2. SmokeShack");
-            System.out.println("3. Cheeseburger");
-            System.out.println("4. Hamburger");
+            for(int i=0; i<menuItems.size(); i++) {
+                System.out.println((i+1)+ ". "+ menuItems.get(i).getMenuName());
+            }
             System.out.println("0. 종료");
 
             chooseMain = sc.nextInt();
 
-            if (chooseMain < 0 || chooseMain > 5) {
+            if (chooseMain < 0 || chooseMain > 4) {
                 System.out.println("잘못 입력 하셨습니다.");
             } else {
-                switch (chooseMain) {
-                    case 1 :
-                        System.out.println("ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
-                        break;
-                    case 2 :
-                        System.out.println("SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
-                        break;
-                    case 3 :
-                        System.out.println("Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
-                        break;
-                    case 4 :
-                        System.out.println("Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
-                        break;
-                    case 0 :
-                        System.out.println("키오스크를 종료합니다.");
-                        turnOff=true;
+                if(chooseMain != 0) {
+                    System.out.println(menuItems.get(chooseMain-1).getMenuName() + "| W "
+                                + menuItems.get(chooseMain-1).getMenuPrice() + " | "
+                                + menuItems.get(chooseMain-1).getMenuExplanation());
+                } else {
+                    System.out.println("키오스크를 종료합니다.");
+                    turnOff=true;
                 }
             }
         } while (!turnOff);
--- src/kioskBurger/MenuItem.java
+++ src/kioskBurger/MenuItem.java
@@ -1,4 +1,25 @@
 package kioskBurger;
 
 public class MenuItem {
+    private String menuName;
+    private double menuPrice;
+    private String menuExplanation;
+
+    public MenuItem(String name, double price, String explan) {
+        this.menuName = name;
+        this.menuPrice = price;
+        this.menuExplanation = explan;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public double getMenuPrice() {
+        return menuPrice;
+    }
+
+    public  String getMenuExplanation() {
+        return menuExplanation;
+    }
 }
