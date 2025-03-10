b79fcd2b34811d107a05c0fcdcbcb91e02b8c024
kickthemoon <soonch67890@gmail>
2025-03-10 17:27:02 +0900

    필수 lv4 임시 저장(1)

--- src/kioskBurger/Kiosk.java
+++ src/kioskBurger/Kiosk.java
@@ -1,66 +1,85 @@
 package kioskBurger;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Kiosk {
 
-    List<MenuItem> menuItems = new ArrayList<>();
-
     Scanner sc = new Scanner(System.in);
 
-    private int chooseMain;
+    private int choose;
     private boolean turnOff = false;
 
-    public Kiosk() {
-        menuItems.add(new MenuItem("ShackBurger   ",6.9,"토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("SmokeShack    ",8.9,"베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("Cheeseburger   ",7.2,"포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("Hamburger   ",5.4,"비프패티를 기반으로 야채가 들어간 기본버거"));
-    }
+    public void start(List<MenuItem> menu){
 
-    public void start(){
         do {
-            System.out.println("");
-            System.out.println("[ SHAKESHACK MENU ]");
-            for(int i=0; i<menuItems.size(); i++) {
-                System.out.println((i+1)+ ". "+ menuItems.get(i).getMenuName());
-            }
+
+            // 메인 메뉴 화면
+            System.out.println("[ MAIN MENU ] ");
+            System.out.println("1. Burgers ");
+            System.out.println("2. Drinks");
+            System.out.println("3. Desserts");
             System.out.println("0. 종료");
 
+            // 메인 메뉴 숫자외 입력 오류처리
             try {
-                chooseMain = sc.nextInt();
+                choose = sc.nextInt();
             } catch (Exception e) {
                 System.out.println("잘못 입력 하셨습니다.");
                 sc.nextLine();
-                continue;
             }
 
-            if (chooseMain < 0 || chooseMain > 4) {
-                System.out.println("잘못 입력 하셨습니다.");
+            if (choose<0 || choose>3) {
+                System.out.println("잘못 입력 하셨습니다."); // 범위 벗어난 숫자 처리
             } else {
-                if(chooseMain != 0) {
-                    while (true) {
-                        System.out.println(menuItems.get(chooseMain - 1).getMenuName() + "| W "
-                                + menuItems.get(chooseMain - 1).getMenuPrice() + " | "
-                                + menuItems.get(chooseMain - 1).getMenuExplanation());
 
-                        System.out.println("0. 뒤로가기");
+                // 0 입력시 종료, 그외 각 카테고리 불러오기
+                if(choose != 0) {
+
+                    // 카테고리 메뉴 보여주기
+                    while (true) {
+                        System.out.println("[ BURGERS MENU ]");
+                        for (int i = 0; i < menu.size(); i++) {
+                            System.out.println((i + 1) + ". " + menu.get(i).getMenuName());
+                        }
+                        System.out.println("0. 되돌아가기");
+
                         try {
-                            if(0 == sc.nextInt()){
-                                break;
-                            }
+                            choose = sc.nextInt();
                         } catch (Exception e) {
+                            System.out.println("잘못 입력 하셨습니다.");
                             sc.nextLine();
+                            continue;
                         }
 
+                        if (choose < 0 || choose > 4) {
+                            System.out.println("잘못 입력 하셨습니다.");
+                        } else {
+                            if (choose != 0) {
+                                // 배열에 있는 값 보여주기
+                                while (true) {
+                                    System.out.println(menu.get(choose - 1).getMenuName() + "| W "
+                                            + menu.get(choose - 1).getMenuPrice() + " | "
+                                            + menu.get(choose - 1).getMenuExplanation());
+                                    System.out.println("0. 뒤로가기");
+
+                                    try {
+                                        if (0 == sc.nextInt()) {
+                                            break;
+                                        }
+                                    } catch (Exception e) {
+                                        sc.nextLine();
+                                    }
+                                }
+                            } else break;
+                        }
                     }
                 } else {
                     System.out.println("키오스크를 종료합니다.");
                     turnOff=true;
                 }
             }
+
         } while (!turnOff);
     }
 }
