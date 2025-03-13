2c678bf0866308c7c2715f446c689050f56a70bd
kickthemoon <soonch67890@gmail>
2025-03-13 17:45:34 +0900

    예외처리 지역 메서드 처리 & 변수명 정리 choose -> select

--- src/kioskBurger/Kiosk.java
+++ src/kioskBurger/Kiosk.java
@@ -9,44 +9,33 @@ public class Kiosk {
     Menu menu = new Menu();
     Cart cart = new Cart();
 
-    private int choose;
-    private int tempChoose;
-    private boolean turnOff = false;
-
     public void start(){
 
+        boolean turnOff = false;
+
         do {
-            while (true) {
-                // 메인 메뉴 화면
-                System.out.println("[ MAIN MENU ] ");
-                System.out.println("1. Burgers ");
-                System.out.println("2. Drinks");
-                System.out.println("3. Desserts");
-                System.out.println("0. 종료");
+            // 메인 메뉴 화면
+            System.out.println("[ MAIN MENU ] ");
+            System.out.println("1. Burgers ");
+            System.out.println("2. Drinks");
+            System.out.println("3. Desserts");
+            System.out.println("0. 종료");
 
-                cart.showOrders();
+            cart.showOrders();
 
-                // 메인 메뉴 숫자외 입력 오류처리
-                try {
-                    choose = sc.nextInt();
-                    break;
-                } catch (Exception e) {
-                    System.out.println("잘못 입력 하셨습니다.");
-                    sc.nextLine();
-                }
-            }
+            int menuSelect = inputValidInt();
 
-            if (choose>=0 && choose<=5) {
+            if (menuSelect>=0 && menuSelect<=5) {
                 // 0 입력시 종료, 그외 각 카테고리 불러오기
-                if(choose>0 && choose<=3) {
-                    menu.setChooseNum(choose);
-                    menu.makeListMenu(choose); // 초이스 값 메뉴클래스로 넘기기
+                if(menuSelect>0 && menuSelect<=3) {
+                    menu.setSelectNum(menuSelect);
+                    menu.makeListMenu(menuSelect); // 선택한 값을 메뉴클래스로 넘기기
 
                     List<MenuItem> menuList = menu.getMenuItem();
 
                     // 버거 음료 디저트 선택 화면 출력
                     while (true) {
-                        System.out.println("[ "+ menuList.get(choose - 1).getMenuCategory() +" MENU ]");
+                        System.out.println("[ "+ menuList.get(menuSelect - 1).getMenuCategory() +" MENU ]");
 
                         // 배열에 메뉴를 저장
                         for (int i = 0; i < menuList.size(); i++) {
@@ -54,36 +43,26 @@ public class Kiosk {
                         }
                         System.out.println("0. 되돌아가기");
 
-                        try {
-                            choose = sc.nextInt();
-                        } catch (Exception e) {
-                            sc.nextLine();
-                            continue;
-                        }
+                        int menuItemSelect = inputValidInt();
 
                         // 버거 음료 디저트 선택 작업 처리
-                        if (choose >= 0 && choose <= menuList.size()) {
-                            if (choose != 0) {
+                        if (menuItemSelect >= 0 && menuItemSelect <= menuList.size()) {
+                            if (menuItemSelect != 0) {
                                 // 배열에 있는 값 보여주기
                                 while (true) {
-                                    System.out.println("[ "+ menuList.get(choose - 1).getMenuCategory() +" ]  "
-                                            + menuList.get(choose - 1).getMenuName() + "| W "
-                                            + menuList.get(choose - 1).getMenuPrice() + " | "
-                                            + menuList.get(choose - 1).getMenuExplanation());
+                                    System.out.println("[ "+ menuList.get(menuItemSelect - 1).getMenuCategory() +" ]  "
+                                            + menuList.get(menuItemSelect - 1).getMenuName() + "| W "
+                                            + menuList.get(menuItemSelect - 1).getMenuPrice() + " | "
+                                            + menuList.get(menuItemSelect - 1).getMenuExplanation());
                                     System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
                                     System.out.println("1. 추가하기            0. 뒤로가기");
 
-                                    try {
-                                        tempChoose = sc.nextInt();
-                                    } catch (Exception e) {
-                                        sc.nextLine();
-                                        continue;
-                                    }
+                                    int cartSelect = inputValidInt();
 
-                                    if (tempChoose != 1) {
+                                    if (cartSelect == 0) {
                                         break;
-                                    } else {
-                                        cart.saveShopCart(menuList.get(choose - 1));
+                                    } else if(cartSelect == 1) {
+                                        cart.saveShopCart(menuList.get(cartSelect - 1));
                                         System.out.println("장바구니에 담았습니다.");
                                         System.out.println("");
                                         break;
@@ -94,10 +73,10 @@ public class Kiosk {
                             }
                         }
                     }
-                } else if(choose>3 && choose<=5){
+                } else if(menuSelect>3 && menuSelect<=5){
                     if(cart.getCartList().size()>0) {
 
-                        switch (choose) {
+                        switch (menuSelect) {
                             case 4:
                                 System.out.println("아래와 같이 주문 하시겠습니까?");
                                 System.out.println("");
@@ -118,9 +97,24 @@ public class Kiosk {
                     }
                 } else {
                     System.out.println("키오스크를 종료합니다.");
-                    turnOff=true;
+                    turnOff=true; // 메인 화면에서 0 입력 받으면 프로그램 종료
                 }
             }
         } while (!turnOff);
+    }
+    // 예외처리 코드
+    public int inputValidInt() {
+        int select = 0;
+
+        while (true) {
+            try {
+                select = sc.nextInt();
+                break;
+            } catch (Exception e) {
+                sc.nextLine();
+                System.out.println("잘못 입력하였습니다.");
+            }
+        }
+        return select;
     }
 }
--- src/kioskBurger/Menu.java
+++ src/kioskBurger/Menu.java
@@ -7,7 +7,7 @@ public class Menu {
 
     private List<MenuItem> menuItems = new ArrayList<>();
     private String category;
-    private int chooseNum;
+    private int selectNum;
 
     public List<MenuItem> getMenuItem() {
         List<MenuItem> tempArray = new ArrayList<>();
@@ -16,8 +16,8 @@ public class Menu {
         return tempArray;
     }
 
-    public void setChooseNum(int chooseNum) {
-        this.chooseNum=chooseNum;
+    public void setSelectNum(int selectNum) {
+        this.selectNum=selectNum;
     }
 
     public void makeListMenu(int num) {
@@ -38,23 +38,23 @@ public class Menu {
     }
 
     public void menuBurgers() {
-        menuItems.add(new MenuItem("ShackBurger  ",6.9,"토마토, 양상추, 쉑소스가 토핑된 치즈버거", inputMenuCategory(chooseNum)));
-        menuItems.add(new MenuItem("SmokeShack  ",8.9,"베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거", inputMenuCategory(chooseNum)));
-        menuItems.add(new MenuItem("Cheeseburger  ",7.2,"포테이토 번과 비프패티, 치즈가 토핑된 치즈버거", inputMenuCategory(chooseNum)));
-        menuItems.add(new MenuItem("Hamburger  ",5.4,"비프패티를 기반으로 야채가 들어간 기본버거", inputMenuCategory(chooseNum)));
+        menuItems.add(new MenuItem("ShackBurger  ",6.9,"토마토, 양상추, 쉑소스가 토핑된 치즈버거", inputMenuCategory(selectNum)));
+        menuItems.add(new MenuItem("SmokeShack  ",8.9,"베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거", inputMenuCategory(selectNum)));
+        menuItems.add(new MenuItem("Cheeseburger  ",7.2,"포테이토 번과 비프패티, 치즈가 토핑된 치즈버거", inputMenuCategory(selectNum)));
+        menuItems.add(new MenuItem("Hamburger  ",5.4,"비프패티를 기반으로 야채가 들어간 기본버거", inputMenuCategory(selectNum)));
     }
 
     public void menuDrigks() {
-        menuItems.add(new MenuItem("C0ca-Cola  ", 1.5, "코카콜라", inputMenuCategory(chooseNum)));
-        menuItems.add(new MenuItem("C0ca-Cola Zero  ", 1.5, "코카콜라 제로", inputMenuCategory(chooseNum)));
-        menuItems.add(new MenuItem("Sprite  ", 1.5, "스프라이트", inputMenuCategory(chooseNum)));
+        menuItems.add(new MenuItem("C0ca-Cola  ", 1.5, "코카콜라", inputMenuCategory(selectNum)));
+        menuItems.add(new MenuItem("C0ca-Cola Zero  ", 1.5, "코카콜라 제로", inputMenuCategory(selectNum)));
+        menuItems.add(new MenuItem("Sprite  ", 1.5, "스프라이트", inputMenuCategory(selectNum)));
     }
 
     public void menuDesserts() {
-        menuItems.add(new MenuItem("French fries  ", 2.3, "감자튀김, 케찹 포함", inputMenuCategory(chooseNum)));
-        menuItems.add(new MenuItem("Cheese French Fries  ", 3.3, "치즈 감자튀김", inputMenuCategory(chooseNum)));
-        menuItems.add(new MenuItem("Chicken Nuggets  ", 3.5, "치킨너겟, 케찹 포함", inputMenuCategory(chooseNum)));
-        menuItems.add(new MenuItem("Corn salad  ", 2.4, "콘 샐러드", inputMenuCategory(chooseNum)));
-        menuItems.add(new MenuItem("Coleslaw  ", 2.4, "코울슬로", inputMenuCategory(chooseNum)));
+        menuItems.add(new MenuItem("French fries  ", 2.3, "감자튀김, 케찹 포함", inputMenuCategory(selectNum)));
+        menuItems.add(new MenuItem("Cheese French Fries  ", 3.3, "치즈 감자튀김", inputMenuCategory(selectNum)));
+        menuItems.add(new MenuItem("Chicken Nuggets  ", 3.5, "치킨너겟, 케찹 포함", inputMenuCategory(selectNum)));
+        menuItems.add(new MenuItem("Corn salad  ", 2.4, "콘 샐러드", inputMenuCategory(selectNum)));
+        menuItems.add(new MenuItem("Coleslaw  ", 2.4, "코울슬로", inputMenuCategory(selectNum)));
     }
 }
