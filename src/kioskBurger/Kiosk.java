3128a483fec5a4bd583409d924cbb64f48913b31
kickthemoon <soonch67890@gmail>
2025-03-14 11:09:58 +0900

    코드 정리 및 열거상수 명명규칙에 따른 명칭 수정

--- src/kioskBurger/Kiosk.java
+++ src/kioskBurger/Kiosk.java
@@ -93,10 +93,10 @@ public class Kiosk {
                                     if (orderSelect == 1) {
                                         // System.out.println("주문이 완료 되었습니다. 금액은 W "+ payment.sumPrice(cart) +" 입니다.");
                                         System.out.println("할인 정보를 입력해주세요.");
-                                        System.out.println("1. "+ Payment.Discounts.nationalMeritoriousPerson.getStatus() +" :  "+ Payment.Discounts.nationalMeritoriousPerson.getRate() +"%");
-                                        System.out.println("2. "+ Payment.Discounts.soldier.getStatus() +"       :   "+ Payment.Discounts.soldier.getRate() +"%");
-                                        System.out.println("3. "+ Payment.Discounts.student.getStatus() +"       :   "+ Payment.Discounts.student.getRate() +"%");
-                                        System.out.println("4. "+ Payment.Discounts.common.getStatus() +"       :   "+ Payment.Discounts.common.getRate() +"%");
+                                        System.out.println("1. "+ Payment.Discounts.NATIOANL_MERITORIOUS_PERSON.getStatus() +" :  "+ Payment.Discounts.NATIOANL_MERITORIOUS_PERSON.getRate() +"%");
+                                        System.out.println("2. "+ Payment.Discounts.SOLDIER.getStatus() +"       :   "+ Payment.Discounts.SOLDIER.getRate() +"%");
+                                        System.out.println("3. "+ Payment.Discounts.STUDENT.getStatus() +"       :   "+ Payment.Discounts.STUDENT.getRate() +"%");
+                                        System.out.println("4. "+ Payment.Discounts.COMMON.getStatus() +"       :   "+ Payment.Discounts.COMMON.getRate() +"%");
 
                                         int statusSelect = inputValidInt();
 
@@ -106,10 +106,10 @@ public class Kiosk {
                                                 Payment.Discounts status = null;
 
                                                 switch (statusSelect) {
-                                                    case 1 -> status = Payment.Discounts.nationalMeritoriousPerson;
-                                                    case 2 -> status = Payment.Discounts.soldier;
-                                                    case 3 -> status = Payment.Discounts.student;
-                                                    case 4 -> status = Payment.Discounts.common;
+                                                    case 1 -> status = Payment.Discounts.NATIOANL_MERITORIOUS_PERSON;
+                                                    case 2 -> status = Payment.Discounts.SOLDIER;
+                                                    case 3 -> status = Payment.Discounts.STUDENT;
+                                                    case 4 -> status = Payment.Discounts.COMMON;
                                                 }
                                                 value = payment.sumPrice(cart)*((100-status.getRate())/100.0);
                                                 System.out.println("주문이 완료 되었습니다. 지불하실 금액은 W "+ value +" 입니다.");
